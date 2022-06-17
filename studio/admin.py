0d39e30f6f50df0d0d0ad432f1269bd5e012eea6
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-17 10:44:35 +0300

    registered classes

--- studio/admin.py
+++ studio/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+# Register your models here.
+
+
+
+
+admin.site.register(Favorite_artist)
+admin.site.register(Tags)
+admin.site.register(Album)
+admin.site.register(Song)
