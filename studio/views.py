5b3945c578e4614aad35603d370bf64dfe3cb924
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-17 14:59:04 +0300

    added index function

--- studio/views.py
+++ studio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Album
 # Create your views here.
 def index(request):
-    return render (request,"index.html")
+    albums = Album.objects.all()
+    return render (request,"index.html",{'albums':albums})
